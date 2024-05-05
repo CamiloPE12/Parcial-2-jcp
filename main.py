8ccb5d5cefb78540ce389d9527db4a6e4922e636
juanca2701 <penaerazocamilo21@gmail.com>
2024-05-04 20:51:55 -0500

    Ultima Actualizacion

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import sqlite3
-# aplicación
+# Aplicación
 app = Flask(__name__)
 
 # Rutas
 @app.route('/')
 def ruta_raiz():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def ruta_index():
+    return render_template('index.html')
+
+@app.route('/productos.html')
+def mostrar_productos():
     try:
-        # Establecer la conexión a la base de datos
         with sqlite3.connect('mi_base_de_datos.db') as conexion:
             cursor = conexion.cursor()
-            # Nombre de la tabla
             nombre_tabla = "ProductosItienda"
-            # Consulta para seleccionar todos los datos de la tabla
             cursor.execute(f"SELECT * FROM {nombre_tabla}")
-            # Obtener todos los resultados
             productos = cursor.fetchall()
-            # Renderizar la plantilla y pasar los datos
             return render_template('productos.html', productos=productos)
     except sqlite3.Error as error:
         print("Error al cargar los datos:", error)
         return "Error al cargar los datos"
 
-@app.route('/producto/<int:pid>')
-def ruta_producto(pid):
-    try:
-        # Establecer la conexión a la base de datos
-        with sqlite3.connect('mi_base_de_datos.db') as conexion:
-            cursor = conexion.cursor()
-            # Nombre de la tabla
-            nombre_tabla = "ProductosItienda"
-            # Consulta para seleccionar un producto específico por su id
-            cursor.execute(f"SELECT * FROM {nombre_tabla} WHERE id = ?", (pid,))
-            # Obtener el resultado
-            producto = cursor.fetchone()
-            # Renderizar la plantilla y pasar el producto
-            return render_template('producto.html', producto=producto)
-    except sqlite3.Error as error:
-        print("Error al cargar el producto:", error)
-        return "Error al cargar el producto"
+@app.route('/contacto.html')
+def ruta_contacto():
+    return render_template('contacto.html')
 
 # Programa principal
 if __name__ == '__main__':
